c8bad45cd64fa6f118ef18d6d99fa8c65cfd69aa
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-07-31 08:13:11 +0530

    Updates

---
+++ precache-manifest.cd9dfd144050ec8d3fffdc9260d25ce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7c7e84dda5709bffc40b06d1c856d8",
+    "url": "/Quote-Generator/index.html"
+  },
+  {
+    "revision": "8918c6cb9df8fb4d7538",
+    "url": "/Quote-Generator/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a1851735fc256935caaf",
+    "url": "/Quote-Generator/static/css/main.96615c80.chunk.css"
+  },
+  {
+    "revision": "8918c6cb9df8fb4d7538",
+    "url": "/Quote-Generator/static/js/2.cd836cbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Generator/static/js/2.cd836cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1851735fc256935caaf",
+    "url": "/Quote-Generator/static/js/main.de548e9b.chunk.js"
+  },
+  {
+    "revision": "68cea268cbb09478a60f",
+    "url": "/Quote-Generator/static/js/runtime-main.8f36aec5.js"
+  }
+]);
